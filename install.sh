9c6237917c53b02f803a93adf9ed4ef70cad815c
moisesjbc <moisesjbc@gmail.com>
2015-05-31 17:30:05 +0100

    Added a man page to script.

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Install script.
 sudo cp ./github-backup.sh /usr/local/bin
 
+# Install man page.
+sudo mkdir /usr/local/man/man8/
+sudo cp man/github-backup.sh.8 /usr/local/man/man8/github-backup.sh.8
+sudo gzip /usr/local/man/man8/github-backup.sh.8
 
+# Reference
+###############################################################################
+# HowTo: Linux / UNIX Create a Manpage - nixCraft
+# http://www.cyberciti.biz/faq/linux-unix-creating-a-manpage/
+###############################################################################
