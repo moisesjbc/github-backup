54646f01c4afa27c628de219691b62f8b9815757
moisesjbc <moisesjbc@gmail.com>
2015-05-31 12:15:56 +0100

    Added checking for number of arguments in Github script.

--- scripts/backup-github-repositories.sh
+++ scripts/backup-github-repositories.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check the number of arguments passed.
+if [ $# -ne 1 ]; then
+    printf "\nERROR: Wrong number of arguments\nUsage: %s <github_user>\n\n" $0
+    exit 1
+fi
+
 # Access info about repositories.
 GITHUB_USER_INFO_FILE="${1}.git"
 curl -o ${GITHUB_USER_INFO_FILE} https://api.github.com/users/${1}/repos
@@ -23,4 +29,13 @@ done
 #
 # Bash For Loop Examples - nixCraft
 # http://www.cyberciti.biz/faq/bash-for-loop/
+#
+# bash shell script check input argument - Stack Overflow
+# http://stackoverflow.com/questions/6482377/bash-shell-script-check-input-argument
+#
+# Escape sequences with “echo -e” in different shells - Unix & Linux Stack Exchange
+# http://unix.stackexchange.com/questions/88307/escape-sequences-with-echo-e-in-different-shells
+#
+# Bash printf syntax basics - LinuxConfig.org
+# http://linuxconfig.org/bash-printf-syntax-basics-with-examples
 ###############################################################################
