f879ccf10cb8407a48485ae9ed154a9d5d4902e5
moisesjbc <moisesjbc@gmail.com>
2015-05-31 17:39:15 +0100

    Script and man page moved to "script" dir.

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Install script.
-sudo install -g 0 -o 0 -m 0755 ./github-backup.sh /usr/local/bin
+sudo install -g 0 -o 0 -m 0755 ./script/github-backup.sh /usr/local/bin
 
 # Install man page.
 sudo mkdir /usr/local/man/man8/
-sudo install -g 0 -o 0 -m 0644 man/github-backup.sh.8 /usr/local/man/man8/
+sudo install -g 0 -o 0 -m 0644 ./script/github-backup.sh.8 /usr/local/man/man8/
 sudo gzip /usr/local/man/man8/github-backup.sh.8
 
 
